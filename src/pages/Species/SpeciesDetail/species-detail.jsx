92a4928cf06572dda5b5d2874ba526e341b1324d
balramsinghdarbar <balramsinghdarbar4@gmail.com>
2026-01-29 16:25:33 +0530

    Update species-detail.jsx

--- src/pages/Species/SpeciesDetail/species-detail.jsx
+++ src/pages/Species/SpeciesDetail/species-detail.jsx
@@ -4,21 +4,8 @@ import Header from "../../../Components/Layout/Header";
 import Footer from "../../../Components/Layout/Footer";
 import Parkbanner from "../../../Components/Comman/park-banner";
 import { useParams } from "react-router-dom";
-// import api from '../../../api/api';
 export default function SpeciesDetail() {
  const { speciesId } = useParams();
-
-// const [speciesId, setSpeciesId] = useState(null);
-
-// useEffect(() => {
-//   api.get("/public/species").then(res => {
-//     const match = res.data.data.find(
-//       item => item.slug === slug
-//     );
-//     setSpeciesId(match?.species_id);
-//   });
-// }, [slug]);
-
 console.log(speciesId); 
 
   return (
